741960880ed9d42758a6b3dc4d4aa31b597ddb34
addhe warman <addhe.warman@gmail.com>
2024-12-30 22:27:25 +0700

    update kill script

--- kill_ts.sh
+++ kill_ts.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # Get the process ID of the run_ts.sh script
-PID=$(pgrep -f "run_ts.sh")
+PID_SCRIPT=$(pgrep -f "run_ts.sh")
+
+# Absolute path to directory where run_ts.sh resides
+RUN_SCRIPT_DIR="/root/awan-ts-bot"
+
+# Get the process ID of main.py started from the specific directory
+PID=$(pgrep -f "${RUN_SCRIPT_DIR}/main.py")
 
 # Check if the process is running and terminate it
 if [ -z "$PID" ]; then
   echo "Trading bot is not running."
 else
+  echo "Killing script runner with PID: $PID_SCRIPT"
+  kill $PID_SCRIPT
   echo "Killing trading bot with PID: $PID"
   kill $PID
 fi
