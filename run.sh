969fe64302921f1ed36f22d7c2f2914e1d0fab2e
addhe warman <addhe.warman@gmail.com>
2024-12-30 20:55:10 +0700

    create run.sh to run the bot

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the path to your Python script
+SCRIPT_PATH="/root/awan-ts-bot/main.py"
+
+# Define logging file
+LOG_FILE="trade_log_spot.log"
+
+# Function to run the bot with logging
+run_bot() {
+  echo "$(date): Starting trading bot" >> $LOG_FILE
+  python3 "$SCRIPT_PATH"
+  EXIT_CODE=$?
+  echo "$(date): Trading bot stopped with exit code $EXIT_CODE" >> $LOG_FILE
+}
+
+# Infinite loop to keep the bot running in case of script failure
+while true; do
+  run_bot
+  # Add a sleep to wait for a few seconds before restart, this avoids immediate restart on failure.
+  echo "$(date): Restarting trading bot after failure" >> $LOG_FILE
+  sleep 10
